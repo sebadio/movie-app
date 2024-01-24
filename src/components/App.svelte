0bdefff86b469fca67cdcde18183911606d43ce1
sebadio <sebastiandiazdopazo@gmail.com>
2024-01-24 19:37:41 -0300

    app svelte now redirects to a movie component instead of tmdb

--- src/components/App.svelte
+++ src/components/App.svelte
@@ -1,16 +1,16 @@
 <script lang="ts">
-  import { getMovies } from "../helpers/getMovies";
-  import Movie from "./Movie.svelte";
+    import { getMovies } from "../helpers/getMovies";
+    import Movie from "./Movie.svelte";
 </script>
 
-<main class="m-auto p-4 w-[1000px] max-w-[calc(100%-2rem) text-xl">
-  <div class="flex flex-wrap justify-around gap-y-8">
-    {#await getMovies()}
-      <p class="w-full min-h-screen text-center">Loading...</p>
-    {:then movies}
-      {#each movies as movie}
-        <Movie {movie} />
-      {/each}
-    {/await}
-  </div>
+<main class="m-auto p-4 w-[1000px] max-w-[calc(100%-2rem)] text-xl">
+    <div class="flex flex-wrap justify-around gap-y-8 max-w-[100vw]">
+        {#await getMovies()}
+            <p class="w-full min-h-screen text-center">Loading...</p>
+        {:then movies}
+            {#each movies as movie}
+                <Movie {movie} />
+            {/each}
+        {/await}
+    </div>
 </main>
