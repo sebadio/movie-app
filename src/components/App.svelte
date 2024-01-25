fbfdbb34211095c3a93932aae0d0eb812a74e4b7
sebadio <sebastiandiazdopazo@gmail.com>
2024-01-24 22:13:48 -0300

    created MoviePosterPlaceholder for main page

--- src/components/App.svelte
+++ src/components/App.svelte
@@ -1,16 +1,32 @@
 <script lang="ts">
     import { getMovies } from "../helpers/getMovies";
     import Movie from "./Movie.svelte";
+    import MoviePosterPlaceholder from "./placeholders/MoviePosterPlaceholder.svelte";
 </script>
 
 <main class="m-auto p-4 w-[1000px] max-w-[calc(100%-2rem)] text-xl">
     <div class="flex flex-wrap justify-around gap-y-8 max-w-[100vw]">
         {#await getMovies()}
-            <p class="w-full min-h-screen text-center">Loading...</p>
+            <MoviePosterPlaceholder />
+            <MoviePosterPlaceholder />
+            <MoviePosterPlaceholder />
+            <MoviePosterPlaceholder />
+            <MoviePosterPlaceholder />
+            <MoviePosterPlaceholder />
+            <MoviePosterPlaceholder />
+            <MoviePosterPlaceholder />
+            <MoviePosterPlaceholder />
+            <MoviePosterPlaceholder />
+            <MoviePosterPlaceholder />
+            <MoviePosterPlaceholder />
         {:then movies}
             {#each movies as movie}
                 <Movie {movie} />
             {/each}
+        {:catch error}
+            <p class="w-full min-h-screen text-center">
+                Error: {error.message}
+            </p>
         {/await}
     </div>
 </main>
---
+++ src/components/placeholders/MoviePosterPlaceholder.svelte
@@ -0,0 +1,9 @@
+<script></script>
+
+<article class="w-[200px] max-h-[400px]">
+    <div class="w-full h-[300px] rounded bg-slate-600 animate-pulse"></div>
+    <div class="w-5/6 h-[2ch] rounded bg-slate-600 animate-pulse mt-1"></div>
+    <div class="w-full h-[1ch] rounded bg-slate-600 animate-pulse mt-2"></div>
+    <div class="w-full h-[1ch] rounded bg-slate-600 animate-pulse mt-1"></div>
+    <div class="w-full h-[1ch] rounded bg-slate-600 animate-pulse mt-1"></div>
+</article>
